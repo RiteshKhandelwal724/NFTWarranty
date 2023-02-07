5674cfc8a65d7480931265ef3a00e2e9c083ebed
Ritesh Kumar Khandelwal <ritesh@Riteshs-MacBook-Air.local>
2023-02-07 16:57:38 +0530

    puching code for web3auth

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -1,13 +1,14 @@
 import { GoogleLogin } from "@react-oauth/google";
-
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
+import jwt_decode from "jwt-decode";
 const GoogleLoginComp = () => {
   return (
     <div>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          console.log("credentialResponse", credentialResponse);
+          console.log(
+            "credentialResponse",
+            jwt_decode(credentialResponse.credential)
+          );
         }}
         onError={() => {
           console.log("Login Failed");
