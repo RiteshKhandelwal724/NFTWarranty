c3b78671316c76b531c06701514eee15c4fd0558
Ritesh Kumar Khandelwal <ritesh@Riteshs-MacBook-Air.local>
2023-03-09 17:05:08 +0530

    Issue for customer login

--- src/components/ConnectBlockChain.js
+++ src/components/ConnectBlockChain.js
@@ -14,12 +14,12 @@ const ConnectBlockChain = () => {
   const loadBlockChainData = async () => {
     // if (typeof window.ethereum !== "undefined") {
     //connecting to metamask
-    const ethProvider = new ethers.providers.Web3Provider(window.ethereum);
-    let accounts = [];
-    if (isConnected) {
-      accounts = await ethProvider.send("eth_requestAccounts", []);
-    }
-    const signer = ethProvider.getSigner();
+    // const ethProvider = new ethers.providers.Web3Provider(window.ethereum);
+    // let accounts = [];
+    // if (isConnected) {
+    //   accounts = await ethProvider.send("eth_requestAccounts", []);
+    // }
+    // const signer = ethProvider.getSigner();
     const provider = new ethers.providers.JsonRpcProvider(
       "https://goerli.infura.io/v3/635b9815bd784fcc9819d8b992250dce"
     );
@@ -30,8 +30,8 @@ const ConnectBlockChain = () => {
     const warrantyCon = new ethers.Contract(
       "0xE48C1327CBf10747D3f13120C39349C8419ed268",
       Warranty.abi,
-      signerNew
       // accounts.length > 0 ? signer : signerNew
+      signerNew
     );
     setWarrantyContract(warrantyCon);
   };
